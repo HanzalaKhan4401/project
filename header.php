f7c94c46baec925b933095d8a874c4ff83cfc112
asp <asp@aptech.com>
2023-10-18 10:48:50 +0500

    work

---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+// session_start();
+// if (!isset($_SESSION['useremail'])) {
+//     header("Location: login.php");
+//     exit();
+// }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+ 
+ <!-- Google Web Fonts -->
+ <link rel="preconnect" href="https://fonts.gstatic.com">
+ <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+
+ <!-- Icon Font Stylesheet -->
+ <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+ <!-- Libraries Stylesheet -->
+ <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+ <link href="lib/animate/animate.min.css" rel="stylesheet">
+ <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+ <link href="lib/twentytwenty/twentytwenty.css" rel="stylesheet" />
+
+ <!-- Customized Bootstrap Stylesheet -->
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+
+ <!-- Template Stylesheet -->
+ <link href="css/style.css" rel="stylesheet">
+
+
+ <style>
+  #form{
+    margin-bottom: 10%;
+  }
+ </style>
+</head>
